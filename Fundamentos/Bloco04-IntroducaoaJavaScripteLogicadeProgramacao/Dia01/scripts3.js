c3ab3a7c875f5b42982681b9eeeefd9579a1fc96
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-03-05 16:24:56 -0300

    Exercicios condiçoes if e else

---
+++ Fundamentos/Bloco04-IntroducaoaJavaScripteLogicadeProgramacao/Dia01/scripts3.js
@@ -0,0 +1,13 @@
+const test = 100;
+
+if (test >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");  
+}
+
+else if (test < 80 && test >= 60) {
+  console.log("Você está na nossa lista de espera.");  
+}
+
+else {
+  console.log("Você foi reprovada(o).");  
+}
