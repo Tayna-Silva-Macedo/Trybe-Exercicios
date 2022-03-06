a83d3410f4bc8d23368dcb2afe92630ba5086345
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-03-06 15:10:05 -0300

    Segundo exercicio

---
+++ Fundamentos/Bloco04-IntroducaoaJavaScripteLogicadeProgramacao/Dia01/Exercicios/scripts2.js
@@ -0,0 +1,10 @@
+const a = 50;
+const b = 100;
+
+if (a > b) {
+  console.log("O maior valor é: " + a);
+} else if (b > a) {
+  console.log("O maior valor é: " + b);
+} else {
+  console.log("Os dois valores são iguais!");
+};
