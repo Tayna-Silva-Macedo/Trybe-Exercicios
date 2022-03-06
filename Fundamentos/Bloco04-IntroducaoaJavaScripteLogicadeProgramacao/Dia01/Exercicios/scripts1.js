4531a4411e9c3adf1c68b6a1d47a41fcbbb6270c
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-03-06 10:36:56 -0300

    Primeiro exercicio

---
+++ Fundamentos/Bloco04-IntroducaoaJavaScripteLogicadeProgramacao/Dia01/Exercicios/scripts1.js
@@ -0,0 +1,14 @@
+const a = 5;
+const b = 7;
+
+const soma = a + b;
+const subtracao = a - b;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = a % b;
+
+console.log("Soma: " + soma)
+console.log("Subtração: " + subtracao)
+console.log("Multiplicação: " + multiplicacao)
+console.log("Divisão: " + divisao)
+console.log("Módulo: " + modulo)
