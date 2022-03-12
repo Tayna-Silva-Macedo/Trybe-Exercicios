e15dfa5ebf36c2ffeee060b00682bc503622a57d
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-03-12 11:52:22 -0300

    Exercicios do conteudo

---
+++ Fundamentos/Bloco05-JavaScript:DOM,EventoseWebStorage/Dia01/Conteudos/scriptQuery.js
@@ -0,0 +1,17 @@
+const paragraph = document.querySelector("#paragraph");
+paragraph.style.color = "red";
+
+document.querySelector("#page-title").innerText = "Lilo & Stitch";
+
+document.querySelector("#second-paragraph").innerText =
+  "Ohana quer dizer família. Família quer dizer nunca abandonar ou esquecer.";
+
+document.querySelector("#subtitle").innerText = "Frase preferida do filme";
+
+let paragrafo = document.querySelector(".paragrafo");
+
+paragrafo.style.fontSize = "50px";
+
+let subtitulo = document.querySelector("h4");
+
+subtitulo.style.color = "orange";
