a9f7ffabb14fcde5029159422cb2de4d668fb197
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-03-15 10:02:29 -0300

    Primeira parte dos exercicios

--- Fundamentos/Bloco05-JavaScript:DOM,EventoseWebStorage/Dia02/Conteudos/Parte III - Removendo Elementos/script.js
+++ Fundamentos/Bloco05-JavaScript:DOM,EventoseWebStorage/Dia02/Conteudos/Parte III - Removendo Elementos/script.js
@@ -1,8 +1,9 @@
-// 1. Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho.
+// // 1. Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho.
 let pai = document.getElementById("pai");
+let filhos = pai.childNodes;
 
-for (let i = pai.childNodes.length - 1; i >= 0; i -= 1) {
-  let filhoAtual = pai.childNodes[i];
+for (let i = filhos.length - 1; i >= 0; i -= 1) {
+  let filhoAtual = filhos[i];
   if (filhoAtual.id !== "elementoOndeVoceEsta") {
     filhoAtual.remove();
   }
