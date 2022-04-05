f40ece3b6346c3e53d59c2d911a295fec2e0731b
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-04-05 18:56:34 -0300

    Exercício 5 dia 7.3

--- Fundamentos/Bloco7-IntroducaoaJavaScriptES6eTestesUnitarios/Dia03/techList.js
+++ Fundamentos/Bloco7-IntroducaoaJavaScriptES6eTestesUnitarios/Dia03/techList.js
@@ -1,15 +1,34 @@
-function techList(array, name) {
-  let novaArray = [];
-  let arrayOrdenada = array.sort();
+// function techList(array, name) {
+//   let novaArray = [];
+//   let arrayOrdenada = array.sort();
 
-  if (array[0] === undefined) {
-    return 'Vazio!';
+//   if (array[0] === undefined) {
+//     return 'Vazio!';
+//   }
+//   for (let i = 0; i < array.length; i += 1) {
+//     novaArray.push({
+//       tech: arrayOrdenada[i],
+//       name,
+//     });
+//   }
+//   return novaArray;
+// }
+
+const techList = (array, nome) => {
+  if (array.length === 0) {
+    return "Vazio!";
   }
-  for (let i = 0; i < array.length; i += 1) {
+
+  const listaOrdenada = array.sort();
+  const novaArray = [];
+
+  for (let i = 0; i < listaOrdenada.length; i += 1) {
     novaArray.push({
-      tech: arrayOrdenada[i],
-      name,
+      tech: listaOrdenada[i],
+      name: nome,
     });
   }
   return novaArray;
-}
+};
+
+module.exports = techList;
