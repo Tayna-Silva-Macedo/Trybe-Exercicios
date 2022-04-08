4126d67ec9ca11969f69cb663dcc0db5f424b2ee
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-04-08 15:15:09 -0300

    Exercício 3

--- Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia02/Exercicios/exercise3.js
+++ Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia02/Exercicios/exercise3.js
@@ -1,60 +1,60 @@
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -66,14 +66,16 @@ const books = [
 const expectedResult = {
   author: {
     birthYear: 1948,
-    name: 'George R. R. Martin',
+    name: "George R. R. Martin",
   },
-  genre: 'Fantasia',
+  genre: "Fantasia",
   id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
+  name: "As Crônicas de Gelo e Fogo",
   releaseYear: 1991,
 };
 
-function getNamedBook() {
-  // escreva seu código aqui
+function getNamedBook(array) {
+  return array.find((elemento) => elemento.name.length === 26);
 }
+
+console.log(getNamedBook(books));
