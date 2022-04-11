e51dcd2da1552e963e4b5c8ddcadb5f7ba17d29e
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-04-11 15:37:05 -0300

    Exercício 7

--- Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia03/Exercícios/exercise7.js
+++ Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia03/Exercícios/exercise7.js
@@ -1,60 +1,60 @@
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -64,8 +64,15 @@ const books = [
 // 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
 // Dica: cada inicial termina com um ponto.
 
-const expectedResult = 'O Senhor dos Anéis';
+const expectedResult = "O Senhor dos Anéis";
 
-function authorWith3DotsOnName() {
-  // escreva seu código aqui
+function authorWith3DotsOnName(arrayLivros) {
+  return arrayLivros.find(
+    (livro) =>
+      livro.author.name[1] === "." &&
+      livro.author.name[4] === "." &&
+      livro.author.name[7] === "."
+  ).name;
 }
+
+console.log(authorWith3DotsOnName(books));
