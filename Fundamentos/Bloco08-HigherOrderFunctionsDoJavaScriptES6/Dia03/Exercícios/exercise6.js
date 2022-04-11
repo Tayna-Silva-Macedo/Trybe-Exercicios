de03f92b23172c82a6ee3239d894fbd4434dc276
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-04-11 15:43:08 -0300

    Exercício 6

--- Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia03/Exercícios/exercise6.js
+++ Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia03/Exercícios/exercise6.js
@@ -1,60 +1,60 @@
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -64,11 +64,19 @@ const books = [
 // 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
 
 const expectedResult = [
-  'O Senhor dos Anéis',
-  'Fundação',
-  'O Chamado de Cthulhu',
+  "O Senhor dos Anéis",
+  "Fundação",
+  "O Chamado de Cthulhu",
 ];
 
-function oldBooks() {
-  // escreva seu código aqui
+function oldBooks(arrayLivros, anoAtual) {
+  const maisDe60 = arrayLivros.filter(
+    (livro) => anoAtual - livro.releaseYear > 60
+  );
+
+  const nomesMaisDe60 = maisDe60.map((livro) => livro.name);
+
+  return nomesMaisDe60;
 }
+
+console.log(oldBooks(books, 2022));
