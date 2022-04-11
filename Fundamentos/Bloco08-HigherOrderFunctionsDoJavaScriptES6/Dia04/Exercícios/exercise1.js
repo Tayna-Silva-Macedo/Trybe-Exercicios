53a2301eeb508b3e46518195bb73a92c95f8002d
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-04-11 20:49:17 -0300

    Exercicio 1

--- Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia04/Exercícios/exercise1.js
+++ Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia04/Exercícios/exercise1.js
@@ -2,6 +2,8 @@ const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
 
 // 1 - Dada uma matriz, transforme em um array.
 
-function flatten() {
-  // escreva seu código aqui
+function flatten(array) {
+  return array.reduce((acumulador, array) => acumulador.concat(array), []);
 }
+
+console.log(flatten(arrays));
