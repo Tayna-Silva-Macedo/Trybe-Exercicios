3d78b7ffb32b22248aace44bcb7e210fccec5d16
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-04-11 11:31:25 -0300

    Exercício 2

--- Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia03/Exercícios/exercise2.js
+++ Fundamentos/Bloco08-HigherOrderFunctionsDoJavaScriptES6/Dia03/Exercícios/exercise2.js
@@ -1,65 +1,105 @@
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
   },
 ];
 
-// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
-// Dica: use as funções map , sort
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// Dica: use as funções map, sort
+
+const expectedResult = [
+  {
+    age: 31,
+    author: "Isaac Asimov",
+  },
+  {
+    age: 38,
+    author: "H. P. Lovecraft",
+  },
+  {
+    age: 39,
+    author: "Stephen King",
+  },
+  {
+    age: 43,
+    author: "George R. R. Martin",
+  },
+  {
+    age: 45,
+    author: "Frank Herbert",
+  },
+  {
+    age: 62,
+    author: "J. R. R. Tolkien",
+  },
+];
+
+function nameAndAge(arrayLivros) {
+  const objeto = arrayLivros.map((livro) => ({
+    ["author"]: livro.author.name,
+    ["age"]: livro.releaseYear - livro.author.birthYear,
+  }));
+
+  const objetoOrdenado = objeto.sort((a, b) => a.age - b.age);
+
+  return objetoOrdenado;
+}
+
+console.log(nameAndAge(books));
