313b532c339dc239fb26de5433826f660b5fdd0d
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-05-08 16:41:22 -0300

    Exercício para fixar dia 11.1

--- 2.Front-end/Bloco11-ComponentesComEstado,EventosEFormulariosComReact/Dia01/Conteudos/fancy-buttons/src/App.js
+++ 2.Front-end/Bloco11-ComponentesComEstado,EventosEFormulariosComReact/Dia01/Conteudos/fancy-buttons/src/App.js
@@ -4,29 +4,91 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
+
     this.handleClickOne = this.handleClickOne.bind(this);
     this.handleClickTwo = this.handleClickTwo.bind(this);
     this.handleClickThree = this.handleClickThree.bind(this);
+
+    this.state = {
+      numeroDeCliquesBotao1: 0,
+      numeroDeCliquesBotao2: 0,
+      numeroDeCliquesBotao3: 0,
+    };
   }
 
   handleClickOne() {
-    console.log('Clicou no botão 1!');
+    this.setState(
+      (estadoAnterior) => ({
+        numeroDeCliquesBotao1: estadoAnterior.numeroDeCliquesBotao1 + 1,
+      }),
+      () =>
+        console.log(
+          `Botão 1 ${this.buttonColor(this.state.numeroDeCliquesBotao1)}`
+        )
+    );
   }
 
   handleClickTwo() {
-    console.log('Clicou no botão 2!');
+    this.setState(
+      (estadoAnterior) => ({
+        numeroDeCliquesBotao2: estadoAnterior.numeroDeCliquesBotao2 + 1,
+      }),
+      () =>
+        console.log(
+          `Botão 2 ${this.buttonColor(this.state.numeroDeCliquesBotao2)}`
+        )
+    );
   }
 
   handleClickThree() {
-    console.log('Clicou no botão 3!');
+    this.setState(
+      (estadoAnterior) => ({
+        numeroDeCliquesBotao3: estadoAnterior.numeroDeCliquesBotao3 + 1,
+      }),
+      () =>
+        console.log(
+          `Botão 3 ${this.buttonColor(this.state.numeroDeCliquesBotao3)}`
+        )
+    );
+  }
+
+  buttonColor(number) {
+    return number % 2 === 0 ? 'green' : 'white';
   }
 
   render() {
+    const {
+      numeroDeCliquesBotao1,
+      numeroDeCliquesBotao2,
+      numeroDeCliquesBotao3,
+    } = this.state;
+
     return (
       <div>
-        <button onClick={this.handleClickOne}>Botão 1</button>
-        <button onClick={this.handleClickTwo}>Botão 2</button>
-        <button onClick={this.handleClickThree}>Botão 3</button>
+        <button
+          onClick={this.handleClickOne}
+          style={{
+            backgroundColor: this.buttonColor(numeroDeCliquesBotao1),
+          }}
+        >
+          Botão 1 | Contador = {numeroDeCliquesBotao1}
+        </button>
+        <button
+          onClick={this.handleClickTwo}
+          style={{
+            backgroundColor: this.buttonColor(numeroDeCliquesBotao2),
+          }}
+        >
+          Botão 2 | Contador = {numeroDeCliquesBotao2}
+        </button>
+        <button
+          onClick={this.handleClickThree}
+          style={{
+            backgroundColor: this.buttonColor(numeroDeCliquesBotao3),
+          }}
+        >
+          Botão 3 | Contador = {numeroDeCliquesBotao3}
+        </button>
       </div>
     );
   }
