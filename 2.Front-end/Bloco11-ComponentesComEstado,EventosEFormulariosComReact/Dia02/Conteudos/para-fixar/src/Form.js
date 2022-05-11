99aaeb3a260400e763264148698e1ba6530affd0
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-05-10 21:10:03 -0300

    Exercício 3, 4, 5 e 6 para fixar

--- 2.Front-end/Bloco11-ComponentesComEstado,EventosEFormulariosComReact/Dia02/Conteudos/para-fixar/src/Form.js
+++ 2.Front-end/Bloco11-ComponentesComEstado,EventosEFormulariosComReact/Dia02/Conteudos/para-fixar/src/Form.js
@@ -2,50 +2,94 @@ import React from 'react';
 
 class Form extends React.Component {
   state = {
+    name: '',
     email: '',
+    age: '',
+    anecdote: '',
+    terms: false,
   };
 
-  handleChange = (event) => {
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
     this.setState({
-      email: event.target.value,
+      [name]: value,
     });
   };
 
   render() {
-    const { email } = this.state;
+    const { name, email, age, anecdote, terms } = this.state;
 
     return (
       <div>
         <h1>Para Fixar</h1>
         <form className='form'>
-          <label htmlFor='name'>
-            Nome:
-            <input id='name' name='name' type='text' />
-          </label>
+          <fieldset>
+            <legend>Informações pessoais</legend>
 
-          <label htmlFor='email'>
-            Email:
+            <label htmlFor='name'>
+              Nome:
+              <input
+                id='name'
+                name='name'
+                type='text'
+                onChange={this.handleChange}
+                value={name}
+              />
+            </label>
+
+            <label htmlFor='email'>
+              Email:
+              <input
+                id='email'
+                name='email'
+                type='email'
+                onChange={this.handleChange}
+                value={email}
+              />
+            </label>
+
+            <label htmlFor='age'>
+              Idade:
+              <select
+                id='age'
+                name='age'
+                onChange={this.handleChange}
+                value={age}
+              >
+                <option value=''>Selecione</option>
+                <option value='adult'>Maior que 18</option>
+                <option value='underage'>Menor que 18</option>
+              </select>
+            </label>
+          </fieldset>
+
+          <fieldset>
+            <legend>Texto e arquivos</legend>
+
+            <label htmlFor='anecdote'>
+              Anedota:
+              <textarea
+                id='anecdote'
+                name='anecdote'
+                onChange={this.handleChange}
+                value={anecdote}
+              />
+            </label>
+
+            <input type='file' />
+          </fieldset>
+
+          <label htmlFor='terms'>
             <input
-              id='email'
-              name='email'
-              type='email'
+              id='terms'
+              name='terms'
+              type='checkbox'
               onChange={this.handleChange}
-              value={email}
+              value={terms}
             />
-          </label>
-
-          <label htmlFor='age'>
-            Idade:
-            <select id='age' name='age' defaultValue=''>
-              <option value=''>Selecione</option>
-              <option value='adult'>Maior que 18</option>
-              <option value='underage'>Menor que 18</option>
-            </select>
-          </label>
-
-          <label htmlFor='anecdote'>
-            Anedota:
-            <textarea id='anecdote' name='anecdote' />
+            Concordo com termos e acordos
           </label>
         </form>
       </div>
