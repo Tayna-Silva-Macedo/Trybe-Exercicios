71d3c3a451c2418cfccaa3bb2a89454ad4f4f1a0
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-05-10 22:35:50 -0300

    Exercício 7, 8 e 9 para fixar

--- 2.Front-end/Bloco11-ComponentesComEstado,EventosEFormulariosComReact/Dia02/Conteudos/para-fixar/src/Form.js
+++ 2.Front-end/Bloco11-ComponentesComEstado,EventosEFormulariosComReact/Dia02/Conteudos/para-fixar/src/Form.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PersonalFieldset from './PersonalFieldset';
+import DataFieldset from './DataFieldset';
 
 class Form extends React.Component {
   state = {
@@ -7,79 +9,60 @@ class Form extends React.Component {
     age: '',
     anecdote: '',
     terms: false,
+    formularioComErros: true,
   };
 
   handleChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => {
+        this.handleError();
+      }
+    );
+  };
+
+  handleError = () => {
+    const { name, email, age, anecdote, terms } = this.state;
+
+    const errorCases = [
+      !name.length,
+      !email.match(/^\S+@\S+$/i),
+      !age.length,
+      !anecdote.length,
+      !terms,
+    ];
+
+    const formularioPreenchido = errorCases.every((error) => error !== true);
+
     this.setState({
-      [name]: value,
+      formularioComErros: !formularioPreenchido,
     });
   };
 
   render() {
-    const { name, email, age, anecdote, terms } = this.state;
+    const { name, email, age, anecdote, terms, formularioComErros } =
+      this.state;
 
     return (
       <div>
         <h1>Para Fixar</h1>
         <form className='form'>
-          <fieldset>
-            <legend>Informações pessoais</legend>
+          <PersonalFieldset
+            nameValue={name}
+            emailValue={email}
+            ageValue={age}
+            handleChange={this.handleChange}
+          />
 
-            <label htmlFor='name'>
-              Nome:
-              <input
-                id='name'
-                name='name'
-                type='text'
-                onChange={this.handleChange}
-                value={name}
-              />
-            </label>
-
-            <label htmlFor='email'>
-              Email:
-              <input
-                id='email'
-                name='email'
-                type='email'
-                onChange={this.handleChange}
-                value={email}
-              />
-            </label>
-
-            <label htmlFor='age'>
-              Idade:
-              <select
-                id='age'
-                name='age'
-                onChange={this.handleChange}
-                value={age}
-              >
-                <option value=''>Selecione</option>
-                <option value='adult'>Maior que 18</option>
-                <option value='underage'>Menor que 18</option>
-              </select>
-            </label>
-          </fieldset>
-
-          <fieldset>
-            <legend>Texto e arquivos</legend>
-
-            <label htmlFor='anecdote'>
-              Anedota:
-              <textarea
-                id='anecdote'
-                name='anecdote'
-                onChange={this.handleChange}
-                value={anecdote}
-              />
-            </label>
-
-            <input type='file' />
-          </fieldset>
+          <DataFieldset
+            anecdoteValue={anecdote}
+            handleChange={this.handleChange}
+          />
 
           <label htmlFor='terms'>
             <input
@@ -92,6 +75,11 @@ class Form extends React.Component {
             Concordo com termos e acordos
           </label>
         </form>
+        {formularioComErros ? (
+          <span style={{ color: 'red' }}>Preencha todos os campos</span>
+        ) : (
+          <span style={{ color: 'green' }}>Todos campos foram preenchidos</span>
+        )}
       </div>
     );
   }
