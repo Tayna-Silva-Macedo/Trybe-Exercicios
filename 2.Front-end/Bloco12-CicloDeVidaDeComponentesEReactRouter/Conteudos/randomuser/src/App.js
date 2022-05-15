da774f3988289a5872a7b83669b2726d0d1f595d
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-05-15 18:07:29 -0300

    Exercício 2 para fixar

---
+++ 2.Front-end/Bloco12-CicloDeVidaDeComponentesEReactRouter/Conteudos/randomuser/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PersonDetails from './components/PersonDetails';
+
+class App extends React.Component {
+  render() {
+    return <div className='App'>
+      <PersonDetails />
+    </div>;
+  }
+}
+
+export default App;
