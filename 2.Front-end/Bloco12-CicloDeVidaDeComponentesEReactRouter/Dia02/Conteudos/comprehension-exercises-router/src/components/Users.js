78fe52b75c915c557866baddb662afc0c5cdb77c
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-05-16 21:42:00 -0300

    Exercícios de fixação

---
+++ 2.Front-end/Bloco12-CicloDeVidaDeComponentesEReactRouter/Dia02/Conteudos/comprehension-exercises-router/src/components/Users.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    const {
+      greetingsMessage,
+      match: {
+        params: { id },
+      },
+    } = this.props;
+
+    return (
+      <div>
+        <h2>Users</h2>
+        <p>
+          {`${greetingsMessage} User ${id}`}, this is my awesome Users component
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Users;
