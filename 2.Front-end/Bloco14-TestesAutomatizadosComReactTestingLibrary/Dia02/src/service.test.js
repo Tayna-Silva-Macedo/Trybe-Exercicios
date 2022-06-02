24d9bd1879a07b95726ff7d26b23234ab3203540
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-06-02 14:52:53 -0300

    Exercício 2

--- 2.Front-end/Bloco14-TestesAutomatizadosComReactTestingLibrary/Dia02/src/service.test.js
+++ 2.Front-end/Bloco14-TestesAutomatizadosComReactTestingLibrary/Dia02/src/service.test.js
@@ -7,3 +7,13 @@ test('Testa se a função foi chamada, qual o seu retorno e quantas vezes foi ch
   expect(service.randomNumber).toHaveBeenCalled();
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
 });
+
+test('Testa se a função foi chamada, qual o seu retorno, quantas vezes foi chamada, com quais parâmetros e se a aplicação da nova implementação acontece apenas uma vez', () => {
+  service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+
+  expect(service.randomNumber(12, 2)).toBe(6);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber).toHaveBeenCalledWith(12, 2);
+  expect(service.randomNumber(12, 2)).toBe(undefined);
+});
