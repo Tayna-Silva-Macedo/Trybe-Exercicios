45bf8abc5a1bfbe9ac720e378ee48168cd634bfe
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-02 11:47:01 -0300

    Exercícios do dia 25.2

---
+++ 3.Back-end/Bloco25-Deployment/Dia02/supabase-with-sequelize/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+
+const { Product } = require('./models');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/product', async (req, res) => {
+  const { name, description } = req.body;
+
+  const product = await Product.create({ name, description });
+
+  return res.status(201).json(product);
+});
+
+app.listen(PORT, () => console.log(`Servidor online na porta ${PORT}`));
