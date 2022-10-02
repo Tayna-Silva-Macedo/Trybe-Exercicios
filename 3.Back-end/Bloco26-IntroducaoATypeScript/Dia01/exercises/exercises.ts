d0473d3a7d003dd97cb65e46efc4edc8f75dbb04
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-02 13:00:41 -0300

    Exercícios para fixar do dia 26.1

---
+++ 3.Back-end/Bloco26-IntroducaoATypeScript/Dia01/exercises/exercises.ts
@@ -0,0 +1,60 @@
+export function greeter(name: string): string {
+  return `Olá ${name}!`;
+}
+
+export function personAge(name: string, age: number): string {
+  return `${name} tem ${age} anos!`;
+}
+
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function sumArray(numbers: number[]): number {
+  return numbers.reduce(add, 0);
+}
+
+export function triangle(base: number, height: number): number {
+  return (base * height) / 2;
+}
+
+export function square(side: number): number {
+  return side ** 2;
+}
+
+export function rectangle(base: number, heigth: number): number {
+  return base * heigth;
+}
+
+export function rhombus(D: number, d: number): number {
+  return (d * D) / 2;
+}
+
+export function trapezoid(B: number, b: number, h: number): number {
+  return ((B + b) * h) / 2;
+}
+
+export function circle(radius: number): number {
+  const PI: number = 3.14;
+  return PI * radius ** 2;
+}
+
+enum DaysOfWeek {
+  Sunday = 1,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+enum RainbowColors {
+  Red = 'Vermelho',
+  Orange = 'Laranja',
+  Yellow = 'Amarelo',
+  Green = 'Verde',
+  Blue = 'Azul',
+  Indigo = 'Anil',
+  Violet = 'Violeta',
+}
