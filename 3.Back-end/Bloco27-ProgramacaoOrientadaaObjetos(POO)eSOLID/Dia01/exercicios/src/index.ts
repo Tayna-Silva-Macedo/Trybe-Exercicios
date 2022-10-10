f2f6f3595572ccab2d35c66450ae30e84cbb09c0
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-10 10:31:08 -0300

    Exercício 01 dia 27.1

---
+++ 3.Back-end/Bloco27-ProgramacaoOrientadaaObjetos(POO)eSOLID/Dia01/exercicios/src/index.ts
@@ -0,0 +1,9 @@
+import Student from './Student';
+
+const student1 = new Student('202001011', 'Maria da Silva');
+
+console.log(student1);
+
+const student2 = new Student('202001012', 'João da Silva');
+
+console.log(student2);
