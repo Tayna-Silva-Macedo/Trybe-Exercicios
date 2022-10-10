31c16c92a3119e11de2bbfd8d163ba7c3a89b864
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-10 11:20:29 -0300

    Exercício 03 dia 27.1

--- 3.Back-end/Bloco27-ProgramacaoOrientadaaObjetos(POO)eSOLID/Dia01/exercicios/src/index.ts
+++ 3.Back-end/Bloco27-ProgramacaoOrientadaaObjetos(POO)eSOLID/Dia01/exercicios/src/index.ts
@@ -1,4 +1,7 @@
 import Student from './Student';
+import Client from './Client';
+import Order from './Order';
+import OrderItem from './OrderItem';
 
 const student1 = new Student('202001011', 'Maria da Silva');
 
@@ -8,3 +11,13 @@ student1.worksGrades = [10, 7];
 console.log(student1);
 console.log('Soma de todas as notas: ', student1.sumGrades());
 console.log('Média de todas as notas: ', student1.calculateAverage());
+
+const client1 = new Client('João');
+
+const sandwich = new OrderItem('Sanduíche Natural', 5.0);
+const juice = new OrderItem('Suco de Abacaxi', 5.0);
+const dessert = new OrderItem('Gelatina de Uva', 2.5);
+
+const order1 = new Order(client1, [sandwich, juice, dessert], 'dinheiro', 0.1);
+
+console.log(order1);
