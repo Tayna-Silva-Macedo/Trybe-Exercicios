a7b59ea1a3eb6da7b3c686a1c9ca11d0719900c8
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-11 18:50:49 -0300

    Exercício 5 dia 27.2

--- 3.Back-end/Bloco27-ProgramacaoOrientadaaObjetos(POO)eSOLID/Dia02/exercicios/src/index.ts
+++ 3.Back-end/Bloco27-ProgramacaoOrientadaaObjetos(POO)eSOLID/Dia02/exercicios/src/index.ts
@@ -2,6 +2,7 @@ import IEmployee from './IEmployee';
 import Person from './Person';
 import Student from './Student';
 import Subject from './Subject';
+import Teacher from './Teacher';
 
 const tayna = new Person('Tayná Silva Macêdo', new Date('1996/05/24'));
 const vania = new Person('Vânia Maria da Silva Macêdo', new Date('1971/05/29'));
@@ -50,9 +51,25 @@ const math = new Subject('Matemática');
 const history = new Subject('História');
 const philosophy = new Subject('Filosofia');
 
-console.log(math);
-console.log(history);
-console.log(philosophy);
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+const joao = new Teacher(
+  'João Antônio da Costa',
+  new Date('1982/04/21'),
+  2000,
+  history
+);
+const lucio = new Teacher(
+  'Lucio Teixeira',
+  new Date('1986/01/29'),
+  2000,
+  philosophy
+);
 
+console.log(marta);
+console.log(joao);
+console.log(lucio);
+
+// const invalidTeacherSalary = new Teacher('Marta da Silva', new Date('1980/03/30'), -2000, math);
+// marta.admissionDate = new Date('2023/10/11');
 
 
