4026e73e168f4cba59877a1aed40cbbf5d77f4af
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-18 10:13:15 -0300

    feat: exercicio 1 resolvido

---
+++ 4.Ciencias-da-computacao/Bloco32-IntroducaoAPython/Dia01/exercicios.py
@@ -0,0 +1,19 @@
+# Exercício 1
+
+
+def higher_number(first_number, second_number):
+    return max(first_number, second_number)
+
+
+print(higher_number(50, 10))
+
+
+# Exercício 2
+
+# Exercício 3
+
+# Exercício 4
+
+# Exercício 5
+
+# Exercício 6
