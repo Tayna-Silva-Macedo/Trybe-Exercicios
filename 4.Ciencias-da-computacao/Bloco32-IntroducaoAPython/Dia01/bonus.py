d8bb60e6727a0c64ddb8a9fcdabc8cac85b0ef37
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-18 15:09:09 -0300

    feat: exercicio bônus 1 resolvido

---
+++ 4.Ciencias-da-computacao/Bloco32-IntroducaoAPython/Dia01/bonus.py
@@ -0,0 +1,15 @@
+# Exercício 1
+
+
+def smallest_element(numbers: list):
+    return min(numbers)
+
+
+print(smallest_element([5, 9, 3, 19, 70, 8, 100, 2, 35, 27]))
+
+
+# Exercício 2
+
+# Exercício 3
+
+# Exercício 4
