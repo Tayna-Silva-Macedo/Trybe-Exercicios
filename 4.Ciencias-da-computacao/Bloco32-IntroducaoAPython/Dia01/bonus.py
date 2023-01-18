541b49876e0f0a3341a083592d0dd5500c553f5a
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-18 15:18:43 -0300

    feat: exercicio bônus 2 resolvido

--- 4.Ciencias-da-computacao/Bloco32-IntroducaoAPython/Dia01/bonus.py
+++ 4.Ciencias-da-computacao/Bloco32-IntroducaoAPython/Dia01/bonus.py
@@ -10,6 +10,14 @@ print(smallest_element([5, 9, 3, 19, 70, 8, 100, 2, 35, 27]))
 
 # Exercício 2
 
+
+def print_triangle(n):
+    for row in range(1, n + 1):
+        print(row * '*')
+
+
+print_triangle(5)
+
 # Exercício 3
 
 # Exercício 4
