13d83dee17e8bbe7346c5a0e44d350be6d40ca4e
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-30 15:32:41 -0300

    feat: segundo exercício resolvido

---
+++ 4.Ciencias-da-computacao/Bloco33-PadroesDeProjeto/Dia03/exercicios/exercicio2.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+
+
+class Alarme:
+    def __init__(self):
+        self.__rotinas = []
+
+    def adicionar_rotinas(self, rotina):
+        self.__rotinas.append(rotina)
+
+    def acionar_todas_rotinas(self):
+        for rotina in self.__rotinas:
+            rotina.acionar()
+
+    def despertar(self):
+        print("Som do alarme: Triiiiiiim, Triiiiiiim")
+        self.acionar_todas_rotinas()
+
+
+class Acionador(ABC):
+    @abstractmethod
+    def acionar(self):
+        raise NotImplementedError
+
+
+class AcionadorLuzes(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Acendendo as luzes")
+
+
+class AcionadorCafeteira(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Preparando o café")
+
+
+class AcionadorComputador(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Ligando o computador")
+
+
+if __name__ == "__main__":
+    alarme_manha = Alarme()
+    AcionadorLuzes(alarme_manha)
+    AcionadorCafeteira(alarme_manha)
+    AcionadorComputador(alarme_manha)
+
+    alarme_manha.despertar()
+
+    alarme_tarde = Alarme()
+    AcionadorCafeteira(alarme_tarde)
+
+    alarme_tarde.despertar()
